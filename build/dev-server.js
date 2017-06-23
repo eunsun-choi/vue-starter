33563be87d96a73a70c35a60a78b82521278941f
Eunsun Mota <emota@roku.com>
2017-06-23 11:38:17 -0700

    :heavy_plus_sign: add `webpack-hot-middleware` and `eventsource-polyfill`

--- build/dev-server.js
+++ build/dev-server.js
@@ -31,6 +31,16 @@ var server = new WebpackDevServer(compiler, {
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 server.app.use(staticPath, express.static('./static'));
 
+var hotMiddleware = require('webpack-hot-middleware')(compiler)
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' });
+    cb()
+  })
+})
+server.app.use(hotMiddleware)
+
 // handle missing things
 server.app.use('*', (req, res) => {
   const extension = path.extname(req.baseUrl);
