574a42e57279c2aa436be19f28120d11abf900fa
Eunsun Mota <emota@roku.com>
2017-06-23 11:19:33 -0700

    :heavy_plus_sign: add `rk-kit`

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import rk from 'rk-kit';
 // import Vuex from 'vuex';
 import App from './App';
 import store from './vuex/store';
 
 import components from './components/';
 
+Vue.use(rk);
 Vue.use(components);
 
 // lumi.theme.use('dark');
